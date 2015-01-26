7f6f60e1b6d64cadb80d0541f8abe22b2f107247
stephomi <stephaneginier@gmail.com>
2015-01-26 01:09:02 +0100

    Adds a method that adds options to a combobox list

--- src/widgets/Combobox.js
+++ src/widgets/Combobox.js
@@ -5,7 +5,7 @@ define([
 
   'use strict';
 
-  var Combobox = function ( valOrObject, callbackOrKey, options) {
+  var Combobox = function (valOrObject, callbackOrKey, options) {
     var value = this._getInitialValue(valOrObject, callbackOrKey);
     var callback = this._getCheckCallback(valOrObject, callbackOrKey);
     options = options || {};
@@ -13,13 +13,7 @@ define([
 
     this.domSelect = document.createElement('select');
     this.domSelect.className = 'gui-select';
-    var keys = Object.keys(options);
-    for (var i = 0; i < keys.length; ++i) {
-      var opt = document.createElement('option');
-      opt.innerHTML = options[keys[i]];
-      opt.value = keys[i];
-      this.domSelect.appendChild(opt);
-    }
+    this.addOptions(options);
 
     this.domSelect.addEventListener('change', this._onChange.bind(this));
     this.setValue(value);
@@ -29,6 +23,15 @@ define([
   Combobox.prototype = {
     _onChange: function (ev) {
       this.setValue(ev.target.value);
+    },
+    addOptions: function (options) {
+      var keys = Object.keys(options);
+      for (var i = 0; i < keys.length; ++i) {
+        var opt = document.createElement('option');
+        opt.innerHTML = options[keys[i]];
+        opt.value = keys[i];
+        this.domSelect.appendChild(opt);
+      }
     },
     setValue: function (val, ignoreCB) {
       this.domSelect.value = val;
